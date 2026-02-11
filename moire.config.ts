a40026d85a30e0c7a6073943b8200b1d85233439
Jianmeng Liang <35825389+Aplusink@users.noreply.github.com>
2026-02-11 17:13:15 -0500

    Update moire.config.ts

--- moire.config.ts
+++ moire.config.ts
@@ -1,10 +1,10 @@
 export const config = {
-  title: "Moire",
-  author: "Aeris",
+  title: "JM的流水账",
+  author: "JM",
   theme: "receipt",
   pageSize: 20,
   utcOffset: 8,
-  description: "Sync your thoughts from Apple Notes by Shortcuts.",
+  description: "这里是我尚未分类的垃圾填埋场。",
   keywords: "memo, thought, sync, apple notes, shortcuts, ios",
   url: "https://moire.blog"
 };
